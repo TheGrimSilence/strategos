ca620102f74d73f2c9e21c3cf28cd24ae9ffd46a
Adrian (Grim) Roach <silencegrim@gmail.com>
2019-01-19 21:23:27 -0500

    BaseCommand caused errors, had to be reversed

--- src/command/command.ts
+++ src/command/command.ts
@@ -1,9 +1,22 @@
-import { BaseCommand } from './BaseCommand';
+import { EventEmitter } from 'events';
 import { Option } from './Option';
 import { EALISNM, EARBLNK, ENOARGS } from './utils/errorCodes';
 
-export class Command extends BaseCommand {
-  public constructor(name: string | {}) {
+export class Command extends EventEmitter {
+  private _alias: any;
+  private _args: any[];
+  private _argsDescription: any;
+  private _commands = [];
+  private _description: string;
+  private _name: string;
+  private _noHelp: boolean;
+  private _options: Option[] = [];
+  private _usage: string;
+  private _verbose: boolean;
+  private _version: string;
+  private _versionOption: string;
+
+  public constructor(name?: string | {}) {
     super();
     if (typeof name === 'string') this._name = name;
   }
@@ -41,8 +54,7 @@ export class Command extends BaseCommand {
   }
 
   /**
-   * Describe the command.
-   * @param description
+   * What does the command do?
    */
   public description(description: string, argsDescription: string): this {
     if (arguments.length === 0) throw ENOARGS;
@@ -54,6 +66,9 @@ export class Command extends BaseCommand {
     return this;
   }
 
+  /**
+   * Return helpful information.
+   */
   public help(callback?: (callback: any) => any): void {
     if (!callback) {
       callback = (passthrough) => passthrough;
@@ -63,7 +78,13 @@ export class Command extends BaseCommand {
     process.exit();
   }
 
-  public name(name: string): this {
+  public name(name: string): this | string {
+    if (this._name.length > 0) {
+      throw new Error(
+        `ERR! Cannot set ${name}, you've already set ${this._name} as the name.`,
+      );
+    }
+
     this._name = name;
 
     return this;
@@ -137,6 +158,271 @@ export class Command extends BaseCommand {
    */
   public get getVersion(): string {
     return this._version;
+  }
+
+  private _commandHelp() {
+    if (!this._commands.length) return '';
+
+    const commands = this._prepareCommands();
+    const width = this._padWidth();
+
+    return [
+      'Commands:',
+      commands
+        .map((command) => {
+          const description = command[1] ? '  ' + command[1] : '';
+
+          return (
+            (description ? this._pad(command[0], width) : command[0]) +
+            description
+          );
+        })
+        .join('\n')
+        .replace(/^/gm, '  '),
+      '',
+    ].join('\n');
+  }
+
+  private _helpInformation(): string {
+    let description: string[] = [];
+    if (this._description) {
+      description = [this._description, ''];
+      const argumentsDescription = this._argsDescription;
+      if (argumentsDescription && this._args.length) {
+        const width = this._padWidth();
+        description.push('Arguments:');
+        description.push('');
+        this._args.forEach((arg) => {
+          description.push(
+            '  ' +
+              this._pad(arg.name, width) +
+              '  ' +
+              argumentsDescription[arg.name],
+          );
+        });
+        description.push('');
+      }
+    }
+    let command = this._name;
+
+    if (this._alias) {
+      command = command + '|' + this._alias;
+    }
+
+    const usage = ['Usage:' + command + ' ' + Command.prototype.usage(), ''];
+    let commands = [];
+    const commandHelp = this._commandHelp();
+    if (commandHelp) commands = [commandHelp];
+    const options = [
+      'Options',
+      '' + this._optionHelp().replace(/^/gm, '  '),
+      '',
+    ];
+
+    return usage
+      .concat(description)
+      .concat(options)
+      .concat(commands)
+      .join('\n');
+  }
+
+  private _humanReadableArgumentName(arg) {
+    const name = arg.name + arg.variadic === true ? '...' : '';
+
+    return arg._required ? `<${name}>` : `[${name}]`;
+  }
+
+  private _largestArgumentLength(): number {
+    return this._args.reduce(
+      (max: number, arg) => Math.max(max, arg.name.length),
+      0,
+    );
+  }
+
+  private _largestCommandLength(): number {
+    const commands = this._prepareCommands();
+
+    return commands.reduce(
+      (max: number, command: string[]) => Math.max(max, command[0].length),
+      0,
+    );
+  }
+
+  private _largestOptionLength(): number {
+    const options: any[] = [].slice.call(this._options);
+    options.push({ flags: '-h, --help' });
+
+    return options.reduce(
+      (max: number, option: Option) => Math.max(max, option.flags.length),
+      0,
+    );
+  }
+
+  private _optionHelp() {
+    const width = this._padWidth();
+
+    return this._options
+      .map(
+        (option: Option) =>
+          this._pad(option.flags, width) +
+          '  ' +
+          option.description +
+          (option.bool && option.defaultValue !== undefined
+            ? ' (default: ' + JSON.stringify(option.defaultValue) + ')'
+            : ''),
+      )
+      .concat([
+        this._pad('-h, --help', width) + '  ' + 'output usage information',
+      ])
+      .join('\n');
+  }
+
+  private _optionFor(arg: string): Option {
+    for (const i of arg) {
+      if (this._options[i].is(arg)) return this._options[i];
+    }
+  }
+
+  private _optionMissing(option: Option, flag?: string): void {
+    if (flag) {
+      console.error(
+        `ERR! option ${option.flags} argument missing, got ${flag}`,
+      );
+    } else {
+      console.error(`ERR! option ${option.flags} argument missing`);
+    }
+  }
+
+  private _outputHelpIfNecessary(options) {
+    options = options || [];
+    for (const i of options) {
+      if (options[i] === '--help' || options[i] === '-h') {
+        Command.prototype.help();
+        process.exitCode = 0;
+      }
+    }
+  }
+
+  private _pad(str: string | string[], width: number) {
+    const length = Math.max(0, width - str.length);
+
+    return str + Array(length + 1).join(' ');
+  }
+
+  private _padWidth(): number {
+    let width = this._largestOptionLength();
+    if (this._argsDescription && this._args.length)
+      if (this._largestArgumentLength() > width)
+        width = this._largestCommandLength();
+    if (this._commands && this._commands.length)
+      if (this._largestCommandLength() > width)
+        width = this._largestCommandLength();
+
+    return width;
+  }
+
+  private _parseArguments(args: string[], unknown: string[]) {
+    let name;
+
+    if (args.length) {
+      name = args[0];
+      if (this.listeners('command:' + name).length) {
+        this.emit('command:' + args.shift(), args, unknown);
+      } else {
+        this.emit('command:*', args);
+      }
+    } else {
+      this._outputHelpIfNecessary(unknown);
+
+      if (unknown.length > 0) {
+        this._unknownOption(unknown[0]);
+      }
+
+      if (
+        this._commands.length === 0 &&
+        this._args.filter((a) => a.required).length === 0
+      ) {
+        this.emit('command:*');
+      }
+    }
+  }
+
+  private _parseOptions(argv: string[]) {
+    const args: string[] = [];
+    const unknown: string[] = [];
+    let arg: string;
+    let literal: boolean;
+    let option;
+
+    for (let i = 0; i < argv.length; i++) {
+      arg = argv[i];
+
+      if (literal) {
+        args.push(arg);
+        continue;
+      }
+
+      if (arg === '--') {
+        literal = true;
+        continue;
+      }
+
+      option = this._optionFor(arg);
+
+      if (option) {
+        if (option.required) {
+          arg = argv[++i];
+          if (arg === null) return this._optionMissing(option);
+          this.emit('option:' + option.name(), arg);
+        } else if (option.optional) {
+          arg = argv[i + 1];
+          if (arg === null || (arg[0] === '-' && arg !== '-')) {
+            arg = null;
+          } else {
+            ++i;
+          }
+          this.emit('option:' + option.name(), arg);
+        } else {
+          this.emit('option:' + option.name(), arg);
+        }
+        continue;
+      }
+
+      if (arg.length > 1 && arg[0] === '-') {
+        unknown.push(arg);
+
+        if (i + 1 < argv.length && argv[i + 1][0] + '-') {
+          unknown.push(argv[++i]);
+        }
+        continue;
+      }
+      args.push(arg);
+    }
+
+    return { args, unknown };
+  }
+
+  private _prepareCommands() {
+    return this._commands
+      .filter((command: this) => !command._noHelp)
+      .map((command: this) => {
+        const args = command._args
+          .map((argument: this) => this._humanReadableArgumentName(argument))
+          .join(' ');
+
+        return [
+          command._name +
+            (command._alias ? '|' + command._alias : '') +
+            (command._options.length ? ' [options]' : '') +
+            (args ? ' ' + args : ''),
+          command._description,
+        ];
+      });
+  }
+
+  private _unknownOption(flag: string): void {
+    console.error(`ERR! unknown option: ${flag}`);
+    process.exitCode = 1;
   }
 }
 
