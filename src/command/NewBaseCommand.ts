e45cdf5b8e9fa02ad5fa0431898718598d535c25
Adrian (Grim) Roach <silencegrim@gmail.com>
2019-01-23 20:51:24 -0500

    Modifications

--- src/command/NewBaseCommand.ts
+++ src/command/NewBaseCommand.ts
@@ -1,22 +1,132 @@
+import chalk from 'chalk';
 import { EventEmitter } from 'events';
+import { inspect } from 'util';
 
 const commands = [];
+const command = { name: '' };
 
 export class BaseCommand extends EventEmitter {
   protected _action: () => any;
   protected _alias: string;
+  protected _args: string[] = [];
   protected _command: ICommand = {};
   protected _commands: ICommand[] = commands;
   protected _description: string;
+  protected _isGlobalScope: boolean;
   protected _name: string;
-  protected _nameExists: boolean;
-  protected _options: IOption[];
+  protected _options: IOption[] = [];
   protected _subCommands: ISubCommand[] = [];
   protected _verbose: boolean;
   protected _verboseFormat: VerboseFormat;
   protected _version: string;
+
   public constructor() {
     super();
+  }
+
+  public findOption(argument: string): IOption {
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this._options.length; i++) {
+      if (
+        (this._options[i].flags.long || this._options[i].flags.short) ===
+        argument
+      )
+        return this._options[i];
+    }
+  }
+
+  protected _buildCommand() {
+    console.log(
+      chalk`{magenta building} ${this._name ? this._name : 'xHoustonCore'}`,
+    );
+    if (!this._name) {
+      this._isGlobalScope = true;
+      this._command.name = 'xHoustonCore';
+      if (this._description) this._command.description = this._description;
+      if (this._options) this._command.options = this._options;
+      if (this._version) this._command.version = this._version;
+    } else {
+      this._command.name = this._name;
+      if (this._action) this._command.action = this._action;
+      if (this._alias) this._command.alias = this._alias;
+      if (this._description) this._command.description = this._description;
+      if (this._options) this._command.options = this._options;
+      if (this._subCommands.length > 0)
+        this._command.subCommands = this._subCommands;
+      if (this._verbose) this._command.verbose = this._verbose;
+      if (this._verboseFormat)
+        this._command.verboseFormat = this._verboseFormat;
+      if (this._version) this._command.version = this._version;
+    }
+    // for (const key in this) {
+    //   if (this.hasOwnProperty(key)) {
+    //     const element = this[key];
+    //     // console.log(key, element);
+    //     if (this[key.replace(/_/, '')] !== (this['domain'] || '_eventsCount'))
+    //       this._command[key.replace(/_/, '')] = this[key];
+    //     console.log(key);
+    //   }
+    // }
+    console.log(
+      inspect(this._command as object, { colors: true, depth: Infinity }),
+    );
+  }
+
+  protected _error(creator: string) {
+    console.error(
+      chalk`{red ERR!} .{blue ${creator}}() called, but not defined.`,
+    );
+    process.exit();
+  }
+
+  protected _isValidCommand(obj: ICommand): boolean {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const element = obj[key];
+        console.log(`${key}: ${element}`);
+        if (element === null) return false;
+      }
+    }
+
+    return true;
+  }
+
+  protected _parseOptions(argv: string[]) {
+    const args: string[] = [];
+    let option: IOption;
+    const unknownOptions: string[] = [];
+
+    for (let i = 0; i < argv.length; i++) {
+      let arg = argv[i];
+
+      option = this.findOption(arg);
+
+      if (option) {
+        if (option.isRequired) {
+          arg = argv[i++];
+          this.emit(`option:${option.flags.long}`, arg);
+        } else if (option.isOptional) {
+          arg = argv[++i];
+          this.emit(`option:${option.flags.long}`, arg);
+        } else {
+          this.emit(`option:${option.flags.long}`);
+        }
+
+        continue;
+      }
+
+      if (arg.length > 1 /*&& arg[0] === '-'*/) {
+        unknownOptions.push(arg);
+
+        if (++i < argv.length /* && argv[++i][0] !== '-'*/)
+          unknownOptions.push(argv[++i]);
+        continue;
+      }
+      
+      args.push(arg);
+    }
+
+    return { args, unknown: unknownOptions };
   }
 }
 
@@ -46,6 +156,9 @@ export interface IOption {
   };
   /** -h, --help */
   option: string;
+  isOptional: string;
+  isRequired: boolean;
+  variadic: string;
 }
 
 export interface ISubCommand {
--- src/command/NewCommand.ts
+++ src/command/NewCommand.ts
@@ -1,57 +1,119 @@
+import chalk from 'chalk';
 import { inspect } from 'util';
 import { BaseCommand, ISubCommand } from './NewBaseCommand';
 
 export class Command extends BaseCommand {
-  public constructor();
-  public constructor(name?: {});
-  public constructor(name?: string, description?: string, action?: () => void) {
+  public constructor(
+    name?: string | ISubCommand,
+    description?: string,
+    action?: () => void,
+  ) {
     super();
     // TODO: if no arguments are passed, assume we're in the global scope, i.e. running Command.parse()
-    if (name === undefined) {
-      console.log('Global scope detected!');
+    if (arguments.length === 0) this._isGlobalScope = true;
+    if (typeof name === 'object') {
+      if (this._isValidCommand(name)) this._commands.push(name);
+      else throw new Error('Woah there fucker');
+    } else {
+      this._name = name;
+      if (description && typeof description === 'function')
+        this._action = description;
+      if (description) this._description = description;
+      if (action) this._action = action;
     }
     // TODO: validate the object and error on missing keys or values
     // TODO: add the information to the command object
-    this._commands.push(this._command);
   }
-
+  /**
+   * Adds an action to the command.
+   *
+   * You can also add an action directly in the `Command()` initialization.
+   */
   public action(callback: () => void): this {
     // TODO: add the callback to the command object and execute on run
-    this._command.action = callback;
+    if (arguments.length === 0) this._error('action');
+    // this._action = callback;
+    const self = this;
+    const listener = (args, unknown) => {
+      args = args || [];
+      unknown = unknown || [];
+      const parsed = this._parseOptions(unknown);
+      self._args.forEach((arg, i) => { });
+      callback.apply(self, args);
+    };
+
+    this.on(`command:${this._name}`, listener);
+    if (this._alias) this.on(`command:${this._alias}`, listener);
 
     return this;
   }
+  /** Adds an alias to the command. */
+  public alias(alias?: string): this {
+    if (arguments.length === 0) this._error('alias');
 
-  public alias(alias: string): this {
-    // TODO: add the alias to the command object
-    this._command.alias = alias;
+    this._alias = alias;
 
     return this;
   }
-
+  /**
+   * Adds a description to the command.
+   *
+   * This is for Commander-based users, I recommend defining the name and description
+   * within the `Command()` initialization.
+   */
   public description(desc: string): this {
     // TODO: add the description to the command object
-    this._command.description = desc;
+    if (arguments.length === 0) this._error('description');
+
+    this._description = desc;
 
     return this;
   }
-
+  /**
+   * Build the command and append it to the list.
+   *
+   * Ends the chain completely, must be called, and at the end of the chain.
+   */
+  public end() {
+    this._buildCommand();
+    this._commands.push(this._command);
+  }
+  /**
+   * Returns helpful information.
+   */
   public help(): this {
     // TODO: output the built help string
 
     return this;
   }
-
+  /**
+   * Adds a name to the command.
+   *
+   * This is for Commander-based users, I recommend defining the name and description
+   * within the `Command()` initialization.
+   */
   public name(name: string): this {
     // TODO: add the name to the command object
-    this._nameExists = true;
-    this._command.name = name;
+    if (arguments.length === 0) this._error('name');
+
+    this._name = name;
 
     return this;
   }
 
-  public option(): this {
+  public option(
+    flags:
+      | string
+      | {
+          long: string;
+          short?: string;
+        },
+    description?: string,
+    fn?: () => void,
+    defaultValue?: string,
+  ): this {
     // TODO: validate and add the option to the command-options object-array
+    if (arguments.length === 0) this._error('option');
 
     return this;
   }
@@ -62,6 +124,7 @@ export class Command extends BaseCommand {
    */
   public options(): this {
     // TODO: validate and add the option-object to the command-options object-array
+    if (arguments.length === 0) this._error('options');
 
     return this;
   }
@@ -82,21 +145,22 @@ export class Command extends BaseCommand {
     description?: string,
     action?: () => void,
   ): this {
+    if (arguments.length === 0) this._error('subCommand');
     if (this._command.subCommands === undefined) {
       console.log('creating subCommand Array');
       this._command.subCommands = [];
     }
     if (typeof name === 'object') {
       // TODO: validate the object and push it to the sub-command object-array
-      this._command.subCommands.push(name as ISubCommand);
+      this._subCommands.push(name as ISubCommand);
     } else {
       // TODO: build sub-command object and push to sub-command object-array
-      this._command.subCommands.push({ name, description, action });
+      this._subCommands.push({ name, description, action });
     }
 
     return this;
   }
-  /** Return usage information */
+  /** Set usage information */
   public usage(): this {
     return this;
   }
@@ -109,6 +173,9 @@ export class Command extends BaseCommand {
   }
   /** Set the command version */
   public version(version: string): this {
+    if (arguments.length === 0) this._error('version');
+    this._version = version;
+
     return this;
   }
   // ! Getters
