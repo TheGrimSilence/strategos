119b55005809f64b4e9d498a9faf4ef72d25573d
Adrian (Grim) Roach <silencegrim@gmail.com>
2019-01-24 18:02:17 -0500

    Alter Internal API to reflect internal code as well as possible outcome

--- src/command/ICommand.ts
+++ src/command/ICommand.ts
@@ -1,21 +1,36 @@
 import { SemVer } from 'semver';
+import { VerboseStyle, VerboseLevel, IVerbose } from './base/verbose';
 
 /**
  * The public api.
  */
-export interface ICommand extends ICommandBase {
+export interface ICommandAPI {
   /**
    * The command alias.
    */
-  alias(alias: string): this;
+  alias: string | ((alias?: string) => this);
   /**
    * Set a callback `fn`
    */
-  action(fn: () => void): this;
+  action: () => void | ((fn: () => void) => this);
   /**
    * The command description.
    */
-  description(desc: string): this;
+  description: string | ((desc: string) => this);
+  /**
+   * Reveals the internal data process.
+   *
+   * Defaults: Xploration, General
+   */
+  verbose: IVerbose | ((style?: VerboseStyle, level?: VerboseLevel) => this);
+  /**
+   * Set the command usage.
+   */
+  usage: string | ((usage?: string) => this);
+  /**
+   * Set the command version.
+   */
+  version: SemVer | ((vers?: SemVer) => this);
   /**
    * Ends a command chain, building it and pushing it to the list.
    */
@@ -24,97 +39,4 @@ export interface ICommand extends ICommandBase {
    * Returns helpful information.
    */
   help(): this;
-  /**
-   * Set the command usage.
-   */
-  usage(usage?: string): this;
-  /**
-   * Reveals the internal data process.
-   *
-   * Defaults: Xploration, General
-   */
-  verbose(style?: VerboseStyle, level?: VerboseLevel): this;
-  /**
-   * Set the command version.
-   */
-  version(vers?: SemVer): this;
-}
-/**
- * The internal api
- */
-interface ICommandBase {
-  _args: IArgDetails[];
-  _command: string;
-  _description: string;
-  _options: IOption[];
-  _subCommands: ISubCommand[];
-}
-
-interface IOption {
-  description: string;
-  flags: {
-    long: string;
-    short: string;
-  };
-  option: string;
-  isOptional: string;
-  isRequired: boolean;
-  variadic: string;
-}
-
-interface ISubCommand {
-  name: string;
-  description: string;
-  action: () => void;
-}
-
-interface IArgDetails {
-  name: string;
-  required: boolean;
-  variadic: boolean;
-}
-
-enum VerboseLevel {
-  /**
-   * Logs general information.
-   *
-   * For example: `<example>`
-   */
-  General,
-  /**
-   * Only logs instantiation calls.
-   *
-   * For example: `Building <command>`
-   */
-  Instantiation,
-}
-
-enum VerboseStyle {
-  /**
-   * Aesthetic logging.
-   *
-   * For example: `{blue •}, {green 🗸}, {red ✗}`
-   *
-   * Currently undesigned.
-   */
-  Xploration,
-  /**
-   * Yarn-based style
-   *
-   * For example: `{blue info}, {green success}, {red error}`
-   *
-   */
-  Yarn,
-  /**
-   * Raspberry Pi boot style.
-   *
-   * For example: `[ {green ok} ], [{red warn}], [{cyan info}]`
-   */
-  RaspPi,
-  /**
-   * npm style.
-   *
-   * For example: `npm {red ERR!} {magenta ${something}}, npm {black.bgYellow WARN}`
-   */
-  npm,
 }
