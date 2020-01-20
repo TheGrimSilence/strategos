735fbd9cce9e199f0530b5fa02da09fcf45aa897
TheGrimSilence <silencegrim@gmail.com>
2020-01-20 18:55:10 -0500

    Additional cleanup

--- src/__tests__/Strategos.test.ts
+++ src/__tests__/Strategos.test.ts
@@ -1,20 +1,4 @@
-import { Strategos } from '../'
-import { exec, ChildProcess } from 'child_process'
-import { resolve } from 'path'
 
 test('Instantiates without errors', () => {
-  expect(new Strategos(['help'], []))
+
 })
-
-// test('"help" is enabled', async () => {
-//   let result: any = await cli(['h'], '.')
-//   expect(result.code).toBe(0)
-// })
-
-function cli(args: string[], cwd: string) {
-  return new Promise(resolve => {
-    exec(`ts-node ${resolve('./StrategosTestScript.ts')} ${args}`, { cwd }, (error, stdout, stderr) => {
-      resolve({ code: error && error.code ? error.code : 0, error, stdout, stderr })
-    })
-  })
-}
